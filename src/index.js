048beceae403f0d416f3a506d8c7764551337a98
alice <alice.nthenge@student.moringaschool.com>
2025-03-15 20:27:50 +0300

    Updated index.html and index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.querySelector("#create-task-form");
+  const taskInput = document.querySelector("#new-task-description");
+  const taskList = document.querySelector("#tasks");
+  const prioritySelect = document.querySelector("#priority");
+
+  taskForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const taskText = taskInput.value.trim();
+      if (taskText === "") return;
+
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskText + " ";
+
+      const priority = prioritySelect.value;
+
+      if (priority === "high") {
+          taskItem.style.color = "red";
+      } else if (priority === "medium") {
+          taskItem.style.color = "orange";
+      } else if (priority === "low") {
+          taskItem.style.color = "green";
+      }
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "🚮";
+
+      deleteButton.addEventListener("click", () => {
+          taskItem.remove();
+      });
+
+      taskItem.appendChild(deleteButton);
+      taskList.appendChild(taskItem);
+
+      taskInput.value = "";
+  });
 });
+
+
